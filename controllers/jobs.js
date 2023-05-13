0b87f49c26739c895aca0ce3155fbf49ad1a77e6
Nicholas Manning <njmanning212@gmail.com>
2023-05-13 10:58:55 -0500

    update deleteNote function, test all functions successfully in postman

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -3,6 +3,7 @@ import { Job } from "../models/job.js"
 const index = async (req, res) => {
   try {
     const jobs = await Job.find({})
+    .sort({ createdAt: 'desc' })
     res.status(200).json(jobs)
   } catch (err) {
     console.log(err)
@@ -12,6 +13,7 @@ const index = async (req, res) => {
 
 const create = async (req, res) => {
   try {
+    req.body.applicant = req.user.profile
     const job = await Job.create(req.body)
     res.status(201).json(job)
   } catch (err) {
@@ -43,7 +45,7 @@ const update = async (req, res) => {
 const createNote = async (req, res) => {
   try {
     const job = await Job.findById(req.params.jobId)
-    job.notes.push(req.body)
+    job.notes.unshift(req.body)
     await job.save()
     res.status(201).json(job)
   } catch (err) {
@@ -55,8 +57,7 @@ const createNote = async (req, res) => {
 const deleteNote = async (req, res) => {
   try {
     const job = await Job.findById(req.params.jobId)
-    const note = job.notes.id(req.params.noteId)
-    note.remove()
+    job.notes.id(req.params.noteId).deleteOne()
     await job.save()
     res.status(200).json(job)
   } catch (err) {
