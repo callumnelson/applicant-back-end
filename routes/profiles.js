99ce590ba51017045d3b62f50f91f6fe110f97fd
Nicholas Manning <njmanning212@gmail.com>
2023-05-13 15:10:04 -0500

    set up profile show router and controller function

--- routes/profiles.js
+++ routes/profiles.js
@@ -10,6 +10,8 @@ const router = Router()
 /*---------- Protected Routes ----------*/
 router.use(decodeUserFromToken)
 router.get('/', checkAuth, profilesCtrl.index)
+router.get('/:profileId', checkAuth, profilesCtrl.show)
 router.put('/:id/add-photo', checkAuth, profilesCtrl.addPhoto)
+
 
 export { router }
